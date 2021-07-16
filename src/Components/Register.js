414007d8b83ea42191a6ab0de93179fdc2339b7b
Esteban <cehernandezco@gmail.com>
2021-07-16 12:31:38 +1000

    testing

--- src/Components/Register.js
+++ src/Components/Register.js
@@ -13,7 +13,7 @@ export function Register ( props ){
             <input className="form-control" type="email" name="email" id="email" placeholder="Email" />
             <label className="form-label" htmlFor="password">Password</label>
             <input className="form-control" type="password" name="password" id="password" placeholder="Password" />
-            <button type="submit" className="btn btn-primary">Register</button>
+            <button type="submit" className="btn btn-primary">Register </button>
         </form>
     )
 }
